24361dbb17b89ffff365ff4c8f75d244c8f169b4
sreeja <sreeja@gmail>
2026-08-26 13:50:26 +0000

    java file data added

---
+++ app.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class BasicCalculator {
+   public static void main(String[] args) {
+       double num1, num2, result;
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the first number:");
+       num1 = sc.nextDouble();
+       System.out.println("Enter the second number:");
+       num2 = sc.nextDouble();
+       System.out.println("Enter an operator (+, -, *, /):");
+       char operator = sc.next().charAt(0);
+       switch (operator) {
+           case '+':
+               result = num1 + num2;
+               break;
+           case '-':
+               result = num1 - num2;
+               break;
+           case '*':
+               result = num1 * num2;
+               break;
+           case '/':
+               if (num2 == 0) {
+                   System.out.println("Error: Division by zero is not allowed.");
+                   return;
+               }
+               result = num1 / num2;
+               break;
+           default:
+               System.out.println("Invalid operator.");
+               return;
+       }
+       System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+   }
+}
